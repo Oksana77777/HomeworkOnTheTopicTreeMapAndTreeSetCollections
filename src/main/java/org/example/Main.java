647b9febb68652c48197cec1f9e6437ace0a1b71
Oksana77777 <https://github.com/Oksana77777/GIT-CP-JAVA-6.git>
2023-03-27 21:36:52 +0300

    Add reodme and class main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Denis", "Fedorov", 14));
+        persons.add(new Person("Natalia", "Bocharova", 63));
+        persons.add(new Person("Vadim", "Momot", 45));
+        persons.add(new Person("Dima", "Mohkov", 47));
+        System.out.println(persons);
+
+        Collections.sort(persons, new PersonComparator());
+
+        System.out.println(persons);
+    }
+}
